8e8664455c10300c35d0ebd718435f4d85aff7ba
Eli Fine <eli88fine@gmail.com>
2025-12-01 14:29:17 +0000

    upload

--- infrastructure/src/infrastructure/program.py
+++ infrastructure/src/infrastructure/program.py
@@ -11,6 +11,7 @@ from ephemeral_pulumi_deploy import get_aws_account_id
 from ephemeral_pulumi_deploy import get_config_str
 from ephemeral_pulumi_deploy.utils import PROTECTED_ENVS
 from pulumi import Output
+from pulumi import Resource
 from pulumi import ResourceOptions
 from pulumi import export
 from pulumi_aws.iam import GetPolicyDocumentStatementArgs
@@ -34,7 +35,8 @@ def _get_mime_type(file_path: Path) -> str:
     return content_type or "application/octet-stream"
 
 
-def _upload_assets_to_s3(*, bucket_id: Output[str], base_dir: Path) -> None:
+def _upload_assets_to_s3(*, bucket_id: Output[str], base_dir: Path) -> list[Resource]:
+    uploads: list[Resource] = []
     for dirpath, _, filenames in os.walk(base_dir):
         for filename in filenames:
             file_path = Path(dirpath) / filename
@@ -45,13 +47,16 @@ def _upload_assets_to_s3(*, bucket_id: Output[str], base_dir: Path) -> None:
             s3_key = os.path.relpath(file_path, base_dir)
             # Since resource names cannot have slashes, we replace them with dashes.
             resource_name = append_resource_suffix(s3_key.replace(os.sep, "-"), max_length=200)
-            _ = BucketObjectv2(
-                resource_name,
-                content_type=_get_mime_type(file_path),
-                bucket=bucket_id,
-                key=s3_key,
-                source=pulumi.FileAsset(str(file_path)),
+            uploads.append(
+                BucketObjectv2(
+                    resource_name,
+                    content_type=_get_mime_type(file_path),
+                    bucket=bucket_id,
+                    key=s3_key,
+                    source=pulumi.FileAsset(str(file_path)),
+                )
             )
+    return uploads
 
 
 def pulumi_program() -> None:
