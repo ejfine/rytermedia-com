dcdadd29bf5767860bd204493d1adee9e0d3e041
Eli Fine <eli88fine@gmail.com>
2025-12-07 20:19:13 +0000

    spelling

--- rytermedia_app/app/components/landing/Blog.vue
+++ rytermedia_app/app/components/landing/Blog.vue
@@ -9,7 +9,7 @@ const { data: posts } = await useAsyncData("index-blogs", () =>
   queryCollection("blog").order("date", "DESC").limit(3).all(),
 );
 if (!posts.value) {
-  throw createError({ statusCode: 404, statusMessage: "blogs posts not found", fatal: true });
+  throw createError({ statusCode: 404, statusMessage: "blog posts not found", fatal: true });
 }
 </script>
 
